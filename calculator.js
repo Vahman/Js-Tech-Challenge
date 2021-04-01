610536fb917cf01997492d7ee7b0c19416e99c2d
Vahman <71049458+Vahman@users.noreply.github.com>
2021-04-01 09:33:14 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var num1=prompt("Enter your first number: ");
+var num2=prompt("Enter your second number: ");
+
+var sum=Number(num1)+Number(num2);
+alert("The sum is: "+sum);
+
+var difference=Number(num1)-Number(num2);
+alert("The difference is: "+difference);
+
+var product=Number(num1)*Number(num2);
+alert("The product is: "+product);
+
+var quotient=Number(num1)/Number(num2);
+alert("The quotient is: "+quotient);
